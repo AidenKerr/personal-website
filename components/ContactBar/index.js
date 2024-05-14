c8a43dd110a4d05f63f3a87577bd7313a5a7c50c
AidenKerr <arkerr2000@gmail.com>
2024-05-14 01:00:04 -0700

    update favicon and resume

--- components/ContactBar/index.js
+++ components/ContactBar/index.js
@@ -1,27 +1,27 @@
 import styles from './ContactBar.module.css';
 
 const Link = ({ href, title }) => (
-  <a
-    className={styles.link}
-    href={`http://www.${href}`}
-    target="_blank"
-    rel="noreferrer"
-  >
-    {title}
-  </a>
+    <a
+        className={styles.link}
+        href={`http://www.${href}`}
+        target='_blank'
+        rel='noreferrer'
+    >
+        {title}
+    </a>
 );
 
 export function ContactBar() {
-  return (
-    <div>
-      <Link href={'linkedin.com/in/aidenkerr'} title={'LinkedIn'} />
-      {' | '}
-      <Link href="github.com/aidenkerr" title={'Github'} />
-      {' | '}
-      <Link
-        href="aidenkerr.com/files/Aiden_Kerr_Resume_Fall_2023.pdf"
-        title={'Resume'}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <Link href={'linkedin.com/in/aidenkerr'} title={'LinkedIn'} />
+            {' | '}
+            <Link href='github.com/aidenkerr' title={'Github'} />
+            {' | '}
+            <Link
+                href='aidenkerr.com/files/Aiden_Kerr_Resume_Fall_2024.pdf'
+                title={'Resume'}
+            />
+        </div>
+    );
 }
